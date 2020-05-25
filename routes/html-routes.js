918286e13eafb4bc44b02180d121f13fe541f97b
NJIB <njibuck@gmail.com>
2020-05-24 23:19:25 -0400

    Rough nav buttons added to Revroutes page.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
@@ -17,43 +17,48 @@ module.exports = function(app) {
   // });
 
   // index route loads view.html
-  app.get('/', function(req, res) {
+  app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/subsegment.html'));
   });
 
   // Route to the cms page
-  app.get('/cms', function(req, res) {
+  app.get('/cms', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/cms.html'));
   });
 
   // Route to the cms page
-  app.get('/sms', function(req, res) {
+  app.get('/sms', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/sms.html'));
   });
 
   // comment route loads comment.html
-  app.get('/comment', function(req, res) {
+  app.get('/comment', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/comment.html'));
   });
 
-    // subsegment route loads subsegment.html
-    app.get('/subsegment', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/subsegment.html'));
-    });
+  // subsegment route loads subsegment.html
+  app.get('/subsegment', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/subsegment.html'));
+  });
 
-    // sgmt_rev_goals route loads sgmt_rev_goals.html
-    app.get('/sgmt_rev_goals', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/sgmt_rev_goals.html'));
-    }); 
+  // sgmt_rev_goals route loads sgmt_rev_goals.html
+  app.get('/sgmt_rev_goals', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/sgmt_rev_goals.html'));
+  });
 
   // persons route loads person-manager.html
-  app.get('/persons', function(req, res) {
+  app.get('/persons', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/person-manager.html'));
   });
 
-    // segments route loads segment-manager.html
-    app.get('/segments', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/segment-manager.html'));
-    });
-  
+  // segments route loads segment-manager.html
+  app.get('/segments', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/segment-manager.html'));
+  });
+
+  // routes_to_revenue route loads routes-to-revenue.html
+  app.get('/revroutes', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/routes-to-revenue.html'));
+  });
+
 };
